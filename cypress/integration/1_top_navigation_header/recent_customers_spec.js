a2cde0db10b75915c0e3a7d1802a94ffe5f9dfd8
Syamasundar Pakala <spakala@ciphercloud.com>
2019-02-19 23:46:06 +0530

    splitted recent customer search test cases

--- cypress/integration/1_top_navigation_header/recent_customers_spec.js
+++ cypress/integration/1_top_navigation_header/recent_customers_spec.js
@@ -9,17 +9,27 @@ context('Recent Customers Search', () => {
 
 	    beforeEach(() => {
 	      cy.server()
+		   cy.route({
+	        method: 'POST',
+	        url: '/api/legacy',
+	      }).as('legacy')
 	      cy.route({
 	        method: 'POST',
 	        url: '/api/legacy/DriveTile/GetNotificationCounts',
 	      }).as('RecentCustomers')
+		  cy.wait('@RecentCustomers', { timeout: 120000});
 	    })
 		
 	    after(function () {
 	    	cy.logoutUI()
 	    }) 
 	  
-	    it('Verification Of Recent Customers Displayed List', function() {	
+	    it('Test 1 - Verify that Recent customer search button exists at the Menu bar', function() {	
+		    //Recent Customers Icon
+			cy.get(TopNavigationHeader.recent_customers_icon).should('be.visible')		 
+		})
+		
+	    it('Test 2 - Validate first eight and next 12 recent customers are displays', function() {	
 		    //Click on the Recent Customers Icon
 			cy.get(TopNavigationHeader.recent_customers_icon).click();
 			
@@ -35,7 +45,73 @@ context('Recent Customers Search', () => {
 		    })			 
 		})
 		
-		it('Verification Name in The Recent Customers Card', function() {
+		it.only('Test 3 - Validate customer card when user clicks on any of the recent customers list who has Open deals', function() {
+			//Click on the Recent Customers Icon
+			cy.get(TopNavigationHeader.recent_customers_icon).click();
+			
+			cy.wait('@RecentCustomers').then((xhr) => {
+			//wait for list display
+			 cy.get(TopNavigationHeader.recent_customers_loading_hide).should('exist')
+				
+				cy.get(TopNavigationHeader.recent_customers_displayed_value).each(($elm) => {
+				cy.wrap($elm).invoke('text').then((text) => {
+				if (text != "No open deal") {
+				cy.wrap($elm).click()
+					//wait for window display
+					cy.get(TopNavigationHeader.customer_card_name).should('exist')
+			 
+				//Verify tabs on the customer card
+			   cy.get(TopNavigationHeader.customer_card_tabs).should(($tab) => {
+				  expect($tab).to.contain('Activity')
+				  expect($tab).to.contain('Conversation')
+				  expect($tab).to.contain('Open')
+				  expect($tab).to.contain('Deals')
+				  expect($tab).to.contain('Value')
+				})
+				
+			   //close the window
+			   cy.get(TopNavigationHeader.close_winodw).click()
+					} 						
+				 })
+				})
+		    })				 
+		})
+		
+		it('Test 4 - Validate customer card when user clicks on any of the recent customers list who does not have Open deals', function() {
+			//Click on the Recent Customers Icon
+			cy.get(TopNavigationHeader.recent_customers_icon).click();
+			
+			cy.wait('@RecentCustomers').then((xhr) => {
+			//wait for list display
+			 cy.get(TopNavigationHeader.recent_customers_loading_hide).should('exist')
+				
+				cy.get(TopNavigationHeader.recent_customers_displayed_value).each(($elm) => {
+				cy.wrap($elm).invoke('text').then((text) => {
+				if (text === "No open deal") {
+				cy.wrap($elm).click()
+					//wait for window display
+					cy.get(TopNavigationHeader.customer_card_name).should('exist')
+			 
+				//Verify tabs on the customer card
+			   cy.get(TopNavigationHeader.customer_card_tabs).should(($tab) => {
+				  expect($tab).to.contain('Activity')
+				  expect($tab).to.contain('Conversation')
+				  expect($tab).to.contain('Deals')
+				  expect($tab).to.contain('Value')
+				})
+				
+			   //close the window
+			   cy.get(TopNavigationHeader.close_winodw).click()
+				 } 			   
+				 })
+				})
+		    })					 
+		})
+		
+		it('Test 5 - Validate customer name in the customer card and recent customer list name', function() {
+			//Click on the Recent Customers Icon
+			cy.get(TopNavigationHeader.recent_customers_icon).click();
+			
 			cy.wait('@RecentCustomers').then((xhr) => {
 			cy.get(TopNavigationHeader.recent_customers_displayed).first().then(($ele) => {
 
@@ -54,16 +130,7 @@ context('Recent Customers Search', () => {
 			 
 			   //verify customer card name
 			   cy.get(TopNavigationHeader.customer_card_name).should('have.text', firstCustomerName)
-			   
-			   //Verify tabs on the customer card
-			   cy.get(TopNavigationHeader.customer_card_tabs).should(($tab) => {
-				  expect($tab).to.contain('Activity')
-				  expect($tab).to.contain('Conversation')
-				  expect($tab).to.contain('Open')
-				  expect($tab).to.contain('Deals')
-				  expect($tab).to.contain('Value')
-				})
-				
+			   			
 			   //close the window
 			   cy.get(TopNavigationHeader.close_winodw).click()
 			   
